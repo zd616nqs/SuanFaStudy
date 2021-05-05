f675ce151111f3c74f2a9e54ebc1883eb263d254
牛青山 <qingshan.niu@ly.com>
2021-05-05 18:47:05 +0800

    添加动态数组

--- 算法笔记/NQSDemo/src/main/java/jeason/App.java
+++ 算法笔记/NQSDemo/src/main/java/jeason/App.java
@@ -1,18 +1,22 @@
 package jeason;
 
 import jeason.TimeTool.Task;
+import jeason.a线性表.a动态数组.Main001;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-
+    public static void main( String[] args ) {
         
-        TimeTool.check("测试", new Task() {
-			public void execute() {
-                System.out.println( "哈哈哈");
-			}
-		});
+        // TimeTool.check("测试", new Task() {
+		// 	public void execute() {
+        //         System.out.println( "哈哈哈");
+		// 	}
+		// });
+
+
+        //************************线性表*******************
+        //----动态数组---------
+        Main001.run();
+
     }
 }
---
+++ 算法笔记/NQSDemo/src/main/java/jeason/a线性表/a动态数组/Main001.java
@@ -0,0 +1,47 @@
+package jeason.a线性表.a数组;
+
+
+public class AppOfArrayList {
+
+	public static void run() {
+		ArrayList<Object> list  = new ArrayList<>();
+		list.add(10);
+		list.add(new Person(10, "Jack"));
+		list.add(22);
+		
+		list.indexOf(new Person(10, "Jack"));
+		
+		System.out.println(list);
+//		ArrayList<Object> persons  = new ArrayList<>();
+//		persons.add(new Person(10, "Jack"));
+//		persons.add(null);
+//		persons.add(new Person(15, "Rose"));
+//		persons.add(null);
+//		persons.add(new Person(12, "James"));
+//		persons.add(null);
+//
+//		System.out.println(persons.indexOf(null));
+	}
+
+	static void test() {
+		// int -> Integer
+	
+		// 所有的类，最终都继承java.lang.Object
+
+		// new是向堆空间申请内存
+		ArrayList<Person> persons  = new ArrayList<>();
+		persons.add(new Person(10, "Jack"));
+		persons.add(new Person(12, "James"));
+		persons.add(new Person(15, "Rose"));
+		persons.clear();
+		persons.add(new Person(22, "abc"));
+		
+		System.out.println(persons);
+		ArrayList<Integer> ints  = new ArrayList<>();
+		ints.add(10);
+		ints.add(10);
+		ints.add(22);
+		ints.add(33);
+		System.out.println(ints);
+	}
+}
